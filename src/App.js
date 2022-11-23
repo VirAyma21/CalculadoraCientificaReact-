94af86897963b347fe24514860c5cee7a174a2f6
VirAyma21 <virginiaymararomero@gmail.com>
2022-11-23 20:54:18 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Calculadora from './componentes/Calculadora';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculadora/>
+    </div>
+  );
+}
+
+export default App;
